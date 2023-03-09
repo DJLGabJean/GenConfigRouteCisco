7df3f5ed71301d6461c62dd9cfe764845a846fa3
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-03-09 23:15:07 +0100

    Version 3 Cisco

--- cisco.sh
+++ cisco.sh
@@ -1,73 +1,9 @@
 #!/bin/bash
 
-
-# function domainLookup {
-#     rep : string
-#     while [$rep != "o" || "O" || "n" || "N"] do
-#         read -p "Voulez-vous activer le no ip domain-lookup (o/n)" rep
-
-#         if $rep = "o" || "O"  
-#         then
-#             configure terminal
-#             no ip domain-lookup
-#             end
-#             # echo "ip domain-lookup activé"
-#             return $rep
-#         else if $rep = "n" || "N" 
-#         then
-#             configure terminal
-#             ip domain-lookup
-#             end
-#             # echo "ip domain-lookup désactivé"
-#             return $rep
-#         else
-#             echo "Veuillez répondre par (O)ui ou par (N)on!"
-#         fi
-#     done 
-#     return $rep
-# }
-
-# function dateConfig {
-#     while $date; do
-#         read -p "Voulez-vous activer la date et l'heure identique du système linux et du routeur cisco (o/n)" date
-
-#         if $date = "o" || "O"  
-#         then
-#             clock set timezone UTC 0
-#             end
-#         elif $date = "n" || "N" 
-#         then
-#             echo "Date et heure identique désactivé"
-#         else
-#             echo "Veuillez répondre par (O)ui ou par (N)on!"
-#         fi
-#         done
-#     return $date
-# }
-
-# function hostnameConfig {
-#     while $hostname; do
-#         read -p "Entrez le nom d'hôte: " hostname
-
-#         if [$hostname =~ SW_UL_[0-9]] 
-#         then
-#             echo "Le format du nom d'hôte est correct!"
-#         elif
-#         then
-#             echo "Le format du nom d'hôte est incorrect!"
-#         else 
-#             echo "Veuillez entrer un nom d'hôte valide!"
-#         fi
-#         done
-#     return $hostname
-# }
-
-#!/bin/bash
-
 function domainLookup {
     rep=""
     while [[ $rep != "o"  && $rep != "O"  && $rep != "n"  && $rep != "N" ]]; do
-        read -p "Voulez-vous activer le no ip domain-lookup (o/n) " rep
+        read -p "Voulez-vous activer le no ip domain-lookup (o/n): " rep
 
         if [[ $rep == "o" || $rep == "O" ]]; then
             echo "Activation de no ip domain-lookup"
@@ -91,7 +27,7 @@ function domainLookup {
 function dateConfig {
     date=""
     while [[ $date != "o" && $date != "O" && $date != "n" && $date != "N" ]]; do
-        read -p "Voulez-vous activer la date et l'heure identique du système Linux et du routeur Cisco (o/n) " date
+        read -p "Voulez-vous activer la date et l'heure identique du système Linux et du routeur Cisco (o/n): " date
 
         if [[ $date == "o" || $date == "O" ]]; then
             echo "Activation de la date et heure identique"
@@ -115,7 +51,11 @@ function hostnameConfig {
         read -p "Entrez le nom d'hôte: " hostname
 
         if [[ $hostname =~ ^SW_UL_[0-9]+$ ]]; then
+            configure terminal
+            hostname $hostname
+            end
             echo "Le format du nom d'hôte est correct!"
+            return $hostname
         else
             echo "Le format du nom d'hôte est incorrect!"
         fi
@@ -123,13 +63,30 @@ function hostnameConfig {
     return $hostname
 }
 
-# function banniereConfig {
+function banniereConfig {
+    banniere=""
+    while [[ ! $banniere =~ ^\#.{0,20}\#$ ]]; do
+        read -p "Entrez une bannière pour votre routeur: " banniere
 
-# }
+        if [[ $banniere =~ ^\#.{0,20}\#$ ]]; then
+            configure terminal
+            banner motd $banniere
+            end
+            echo "Le format de la bannière est correct!"
+            return $banniere
+        else
+            echo "Le format de la bannière est incorrect!"
+        fi
+    done
+    return $banniere
+}
+
 
 domainLookup
 dateConfig
 hostnameConfig
+banniereConfig
+
 
 
 
